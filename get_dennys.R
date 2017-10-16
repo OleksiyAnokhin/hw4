bacfe7f8a3c27e984111c06e89128a4caafd2b76
Colin Rundel <rundel@gmail.com>
2017-10-16 15:22:03 -0400

    dennys example

---
+++ get_dennys.R
@@ -0,0 +1,4 @@
+base_url = "http://www2.stat.duke.edu/~cr173/dennys/locations.dennys.com/"
+
+#dir.create("data/dennys/",showWarnings = FALSE, recursive = TRUE)
+#download.file(url, destfile = paste0("data/dennys/",basename(url),".html"), quiet = TRUE)
